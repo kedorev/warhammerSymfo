d7c45e6860b19e75f935c52607674e5c5a20793c
kedorev <kedorev@gmail.com>
2017-11-23 15:24:42 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
-vendor/bin/phpunit >> var
-echo var
-if [ var == 0 ]
+vendor/bin/phpunit
+if [ $? == 0 ]
 then
-    vendor/bin/behat >> var
-    echo var
-    if [ var == 0 ]
+    vendor/bin/beha
+    if [ $? == 0 ]
     then
         vendor/bin/phploc src >> var
-        echo var
-        if [ var == 0 ]
+        if [ $? == 0 ]
         then
             vendor/bin/phpcs ./src >> var
-            echo var
-            if [ var == 0 ]
+            if [ $? == 0 ]
             then
                 php rocketeer.phar deploy
             fi
