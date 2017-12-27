165fff86eee50a3ebe84c81cd9a9c6254d9da611
kedorev <kedorev@gmail.com>
2017-12-27 17:54:23 +0100

    update body

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+vendor/bin/phpunit
+if [ $? -eq 0 ]
+then
+    vendor/bin/phploc src
+    if [ $? -eq 0 ]
+    then
+        #vendor/bin/behat
+        if [ $? -eq 0 ]
+        then
+            #vendor/bin/phpcs ./src
+            if [ $? -eq 0 ]
+            then
+                php rocketeer.phar deploy
+            fi
+        else
+            exit 1
+        fi
+    else
+        exit 1
+    fi
+else
+    exit 1
+fi
